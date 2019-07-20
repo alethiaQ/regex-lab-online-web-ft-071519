6dd2fc275af8338627ef37f181117a70c5be1304
Alethia Quintero <alethiaq22@gmail.com>
2019-07-20 17:03:35 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,8 @@
 def starts_with_a_vowel?(word)
 if word.scan(/^[aeiouAEIOU]\w+ /) return true
-else return false
+else
+  return false
+end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
